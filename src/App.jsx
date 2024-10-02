554d22a79d7a8ed8c2bed7c423d545584abe6fb1
kelvinguetta1608 <kevinvelasco1608@gmail.com>
2024-10-02 01:09:38 -0500

    Planteamiento de la estructura básica del website, incluyendo el html, React, Vite y tailwindcss, además de los componentes para la visualización

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import About from "./Components/About"
+import Banner from "./Components/Banner"
+import Contact from "./Components/Contact"
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Menu from "./Components/Menu"
+import Projects from "./Components/Projects"
+import Skills from "./Components/Skills"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Banner/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+      <Footer/>
+      <Menu/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header =()=>{
+    return(
+        <div className="p-20 py-10 text-xl">
+            <h2>Portfolio</h2>
+        </div>
+    );
+};
+
+export default Header
