5204f72c02847f15e7ed6c9ea6798dbf74c0cef6
KEVIN ANDRES VELASCO PEREZ <kevin.velasco@uao.edu.co>
2024-10-02 14:16:27 -0500

    Selección de iconos para la barra del menú

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,7 +1,57 @@
+import {Link} from "react-scroll";
+import {IoHome, IoPerson, IoBagSharp} from "react-icons/io5"
+import { BsLightbulbFill } from "react-icons/bs";
+
 const Menu =()=>{
     return(
         <div>
-            
+            <div className="flex justify-center flex-col items-center">
+                <Link activeClass="bg=[#1484da] rounded-full" 
+                    to="Home"
+                    smooth={true}
+                    spy={true}
+                    offset={50}
+                    duration={500}
+
+                >
+
+                    <IoHome className="text-[26px] m-[10px] text-center"/>
+                </Link>
+                <Link activeClass="bg=[#1484da] rounded-full" 
+                    to="Home"
+                    smooth={true}
+                    spy={true}
+                    offset={50}
+                    duration={500}
+
+                >
+
+                    <IoPerson className="text-[26px] m-[10px] text-center"/>
+                </Link>
+                <Link activeClass="bg=[#1484da] rounded-full" 
+                    to="Home"
+                    smooth={true}
+                    spy={true}
+                    offset={50}
+                    duration={500}
+
+                >
+
+                    <IoBagSharp className="text-[26px] m-[10px] text-center"/>
+                </Link>
+                <Link activeClass="bg=[#1484da] rounded-full" 
+                    to="Home"
+                    smooth={true}
+                    spy={true}
+                    offset={50}
+                    duration={500}
+
+                >
+
+                    <BsLightbulbFill className="text-[26px] m-[10px] text-center"/>
+                </Link>
+
+            </div>
         </div>
     );
 };
