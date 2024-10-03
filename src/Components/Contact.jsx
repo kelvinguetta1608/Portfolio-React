23d9812910c148cf7b79756d80d70b6d807f683f
kelvinguetta1608 <kevinvelasco1608@gmail.com>
2024-10-03 01:37:59 -0500

    Corrección estilos componente Skills, creación de formulario básico del componente Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,24 @@
 const Contact =()=>{
     return(
-        <div id="Contact">
-
+        <div className="text-white bg-black lg:p-44 p-4" id="Contact">
+            <h2 className="text-4xl text-white font-bold mb-10 text-center">Contact</h2>
+            <div className="lg:w-[600px] md:w-[500px] mx-auto">
+                <div className="relative mt-5">
+                    <input className="block px-2 pb-2.5 pt-4 w-full text-sm bg-transparent w-90 rounded-lg border-2 border-white" type="text" />
+                    <label className="absolute -translate-y-5 z-10 top-2 origin-[0] bg-black mx-4 px-2 text-white text-[16px]" htmlFor="">Your Name</label>
+                </div>
+                <div className="relative mt-5">
+                    <input className="block px-2 pb-2.5 pt-4 w-full text-sm bg-transparent w-90 rounded-lg border-2 border-white" type="email" />
+                    <label className="absolute -translate-y-5 z-10 top-2 origin-[0] bg-black mx-4 px-2 text-white text-[16px]" htmlFor="">Your Email</label>
+                </div>
+                <div className="relative mt-5">
+                    <textarea cols="30" rows="10" className="block px-2 pb-2.5 pt-4 w-full text-sm bg-transparent w-90 rounded-lg border-2 border-white" type="text" />
+                    <label className="absolute -translate-y-5 z-10 top-2 origin-[0] bg-black mx-4 px-2 text-white text-[16px]" htmlFor="">Your Message</label>
+                </div>
+                <div className="flex justify-center">
+                    <button className="px-[20px] py-[6px] text-xl rounded mt-6 bg-[#1484da] mb-10 hover:border transition-all duration-700 ease-in-out hover:bg-transparent border-[#1484da]">Send</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,38 +1,38 @@
 const Skills =()=>{
     return(
         <div className="py-44 lg:px-64 md:px-28 bg-black" id="Skills">
-            <h1 className="text-4xl text-white text-blod mb-10 text-center">Skills</h1>
+            <h1 className="text-4xl text-white font-bold mb-10 text-center">Skills</h1>
             <div className="max-w-7xl mx-auto w-full grid grid-cols-2 gap-4 px-2">
                 {/* Columna de habilidades duras */}
                 <div className="w-full h-full">
                     <div className="w-full h-full p-2">
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Hard</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Programación</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Hard</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Programación</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Conocimiento en lenguajes como C# (para desarrollo de videojuegos en Unity) y Python (para automatización, análisis de datos y desarrollo de aplicaciones).
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Hard</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Unity (2D/3D, AR, VR)</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Hard</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Unity (2D/3D, AR, VR)</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Desarrollo de experiencias interactivas y simulaciones en distintas plataformas.
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Hard</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">UX/UI Design</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Hard</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">UX/UI Design</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Diseño de interfaces y experiencias de usuario enfocadas en la interacción y accesibilidad.
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Hard</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Desarrollo web</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Hard</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Desarrollo web</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Creación de sitios y aplicaciones web utilizando tecnologías como HTML, CSS, JavaScript y React.
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Hard</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Suite Adobe (Photoshop, Illustrator, After Effects)</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Hard</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Suite Adobe (Photoshop, Illustrator, After Effects)</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Diseño gráfico y producción multimedia avanzada, como ilustraciones, animaciones y producción de motion graphics impactantes.
                         </p>
                     </div>
@@ -45,33 +45,33 @@ const Skills =()=>{
                     <div className="absolute left-[-6px] top-1/2 transform -translate-y-1/2 w-3 h-3 rounded-full bg-[#1484da] z-10 text-white text-center"></div>
 
                     <div className="w-full h-full p-2 pl-8" >
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Soft</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Adaptabilidad</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Soft</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Adaptabilidad</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Capacidad para ajustarse rápidamente a nuevas herramientas y tecnologías.
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Soft</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Trabajo en equipo</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Soft</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Trabajo en equipo</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Capacidad para colaborar de manera efectiva en equipos multidisciplinarios, promoviendo un ambiente de cooperación y comunicación.
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Soft</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Liderazgo</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Soft</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Liderazgo</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Competencia para dirigir y motivar equipos en proyectos colaborativos, fomentando un ambiente de trabajo positivo y productivo
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Soft</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Aprendizaje continuo</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Soft</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Aprendizaje continuo</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Enfoque proactivo hacia el desarrollo personal y profesional, siempre en busca de nuevas competencias y mejoras en las habilidades existentes.
                         </p>
 
-                        <h3 className="mb-1 text-sm font-semibold leading-none text-gray-400">Soft</h3>
-                        <h3 className="text-lg font-semibold text-gray-200 my-2">Habilidades de Comunicación</h3>
-                        <p className="mb-4 text-base font-normal text-gray-400">
+                        <h3 className="mb-1 text-sm font-semibold leading-none text-[#1484da]">Soft</h3>
+                        <h3 className="text-lg font-semibold text-white my-2">Habilidades de Comunicación</h3>
+                        <p className="mb-4 text-base font-normal text-gray-300">
                             Capacidad para expresarse claramente y colaborar eficazmente con equipos de trabajo.
                         </p>
                     </div>
