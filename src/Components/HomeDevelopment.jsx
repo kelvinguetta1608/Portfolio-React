b0382ecba87a2d892a023222892ad77737188736
kelvinguetta1608 <kevinvelasco1608@gmail.com>
2024-11-21 23:45:15 -0500

    prueba 2

--- src/Components/HomeDevelopment.jsx
+++ src/Components/HomeDevelopment.jsx
@@ -31,7 +31,7 @@ const HomeDevelopment = () => {
                         <iframe
                             width="100%"
                             height="100%"
-                            src="https://www.youtube.com/watch?v=6vPhcRew8hA/<ID_DEL_VIDEO>"
+                            src="https://www.youtube.com/embed/6vPhcRew8hA"
                             title="YouTube video player"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
